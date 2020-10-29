8939c91890bc27814ddf2fb9b922a5fbf47cdfca
Opilane TTHK <opilane@tthk.ee>
2020-10-29 11:26:30 +0200

    all hell breaking loose

---
+++ Wishlist/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Wishlist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Name three things you would like to recieve as a present.");
+            string UserInput = Console.ReadLine().ToLower();
+            string filePath = @"C:\Users\opilane\Data\wishlist.txt";
+            List<string> dataFromFile = File.ReadAllLines(filePath).ToList();
+
+
+
+            dataFromFile.Add(UserInput);
+            File.WriteAllLines(filePath, dataFromFile);
+
+        }
+    }
+}
